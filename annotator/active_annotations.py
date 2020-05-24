271826cc0f612e14a61c197fc9adb0e198270513
ieriii <enrico.alemani@gmail.com>
2020-05-24 13:29:54 +0100

    Fixed cold start

--- annotator/active_annotations.py
+++ annotator/active_annotations.py
@@ -1,8 +1,9 @@
 import spacy
 import re
 import random
+import numpy as np
 from collections import defaultdict
-from annotator.annotate_utils import filter_spans
+from annotator.annotator_utils import filter_spans
 from IPython.display import display, clear_output
 from ipywidgets import Button, HTML, HBox, Textarea, Output, Layout
 
@@ -12,7 +13,7 @@ def annotate(df,
              strata=None,
              model=None,
              labels=None,
-             regex_flags = 0,
+             regex_flags=0,
              shuffle=False,
              include_skip=True,
              display_fn=display):
@@ -29,6 +30,7 @@ def annotate(df,
     model = spaCy model
     labels: list(any), list of NER labels
     regex_flags: regex, one or more regex flag to apply to re.compile. (e.g. re.I|re.DOTALL). Default: no flags
+    shuffle: bool, option to shuffle data
     include_skip: bool, include option to skip example while annotating
     display_fn: func, function for displaying an example to the user
     
@@ -46,7 +48,7 @@ def annotate(df,
     
     if model is not None:
         nlp = spacy.load(model)  
-    
+
     if strata is not None:
         assert sum([v for k,v in strata.items() if k !='key']) == 1, 'The sum of proportions in strata is different from 1'
         sample = (df
@@ -56,10 +58,13 @@ def annotate(df,
         
     else:
         sample = df.sample(frac=sample_size).reset_index(drop=True)
-    
+        
     if shuffle==True:
         sample = sample.sample(frac=1).reset_index(drop=True)
-    
+
+    if 'annotations' not in sample.columns:
+        sample['annotations'] = ''
+
     def set_label_text():
         nonlocal count_label
         count_label.value = f'{current_index} examples annotated, {len(sample) - current_index} examples left'
@@ -67,7 +72,7 @@ def annotate(df,
     def reset_textarea():
         value = (';\n'.join(f'{label}: insert' for label in labels)+';')
         return value
-    
+
     def show_next():
         nonlocal current_index
         current_index += 1
@@ -148,8 +153,7 @@ def annotate(df,
         entities = {'entities': spans}
         
         # Add entities to dataframe
-#         sample['annotations'][current_index] = (sample[col_text][current_index], entities)
-        sample.at[current_index, 'annotations'] = (sample[col_text][current_index], entities)
+        df.at[current_index, 'annotations'] = (df[col_text][current_index], entities)
   
         show_next()
 
@@ -162,7 +166,7 @@ def annotate(df,
         return
 
     count_label = HTML()
-    print('\033[1mInstructions\033[0m \nInput must be in the following format: \nlabelA: item1, item2; \nlabelB: itemX, itemZ; \n\nIf no entities in text, leave as is and press submit. \nSimilarly, if no entities for a particular label, leave as is (or delete the line for that label). \n\nButtons: \n*submit inserts new annotation (or overwrites existing one if one is present). \n*skip moves forward and leaves NaN (or existing annotation if one is present). \n*finish terminates the annotation session.')
+    print('\033[1mInstructions\033[0m \nInput must be in the following format: \nlabelA: item1, item2; \nlabelB: itemX, itemZ; \n\nIf no entities in text, leave as is and press submit. \nSimilarly, if no entities for a particular label, leave as is (or delete the line for that label). \n\nButtons: \n*submit inserts new annotation (or overwrites existing one if one is present). \n*skip moves forward and leaves empty string (or existing annotation if one is present). \n*finish terminates the annotation session.')
     set_label_text()
     display(count_label)
 
@@ -180,7 +184,7 @@ def annotate(df,
     # Buttons
     btn = Button(description='submit')
     def on_click(btn):
-        add_annotation(df, ta.value, regex_flags)
+        add_annotation(sample, ta.value, regex_flags)
         ta.value = reset_textarea()
     btn.on_click(on_click)
     buttons.append(btn)
