327c0371cdaa3629cc0e7b62019ee3d5642f1dbb
ieriii <enrico.alemani@gmail.com>
2020-11-17 16:32:44 +0000

    Fix import

--- spacy_annotator/__init__.py
+++ spacy_annotator/__init__.py
@@ -1,2 +1,2 @@
-from annotator.active_annotations import annotate
-from annotator.static_annotations import annotate
+from spacy_annotator.active_annotations import annotate
+from spacy_annotator.static_annotations import annotate
