665027053b0009f07e8cbe13ed3aff411e7098e5
ieriii <enrico.alemani@gmail.com>
2020-11-18 16:29:00 +0000

    Change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'spacy_annotator',
   packages=['spacy_annotator'],
-  version = '1.0',
+  version = '1.1',
   license='MIT',
   description = 'SpaCy annotator for Named Entity Recognition (NER) using ipywidgets.',
   author = 'Enrico Alemani',
