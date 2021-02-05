4a27af974d688ed4b4332bfaf8f83e3ca6f83729
user85 <user.85@email.com>
2021-02-05 16:33:25 +0000

    black all

--- setup.py
+++ setup.py
@@ -2,30 +2,31 @@ from setuptools import setup
 
 # Read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name = 'spacy_annotator',
-    packages=['spacy_annotator'],
-    version = '2.0.0',
-    license='MIT',
-    description = 'SpaCy annotator for Named Entity Recognition (NER) using ipywidgets.',
-    author = 'Enrico Alemani',
-    author_email = 'enrico.alemani@hey.com',
-    url = 'https://github.com/ieriii/spacy-annotator',
-    download_url = 'https://github.com/ieriii/spacy-annotator/archive/v_2.0.0.tar.gz',
-    keywords = ['spacy', 'NER', 'NLP'],
+    name="spacy_annotator",
+    packages=["spacy_annotator"],
+    version="2.0.0",
+    license="MIT",
+    description="SpaCy annotator for Named Entity Recognition (NER) using ipywidgets.",
+    author="Enrico Alemani",
+    author_email="enrico.alemani@hey.com",
+    url="https://github.com/ieriii/spacy-annotator",
+    download_url="https://github.com/ieriii/spacy-annotator/archive/v_2.0.0.tar.gz",
+    keywords=["spacy", "NER", "NLP"],
     install_requires=[
-        'ipython',
-        'ipywidgets>=7.5.1',
-        'numpy',
-        'pandas',
-        'spacy',
-      ],
+        "ipython",
+        "ipywidgets>=7.5.1",
+        "numpy",
+        "pandas",
+        "spacy",
+    ],
     zip_safe=False,
     # Enable pypi description
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
 )
--- spacy_annotator/__init__.py
+++ spacy_annotator/__init__.py
@@ -2,8 +2,10 @@
 # from collections import defaultdict
 from IPython.display import clear_output, display, display_html
 from ipywidgets import Button, HTML, HBox, Text, Output, Layout
+
 # import numpy as np
 import pandas as pd
+
 # import re
 # import random
 import spacy
@@ -11,7 +13,8 @@ from spacy import displacy
 from spacy.matcher import PhraseMatcher
 from spacy.tokens import Span
 
-class Annotator: # TODO (object) ?
+
+class Annotator:  # TODO (object) ?
     # TODO clean up
     """
     Build an interactive widget for annotating a list of input examples.
@@ -29,19 +32,19 @@ class Annotator: # TODO (object) ?
     delimiter: str, delimiter to separate entities in GUI. Default: ',' (i.e. comma)
     include_skip: bool, include option to skip example while annotating
     display_fn: func, function for displaying an example to the user
-    
+
     Returns
     -------
     annotations (list): list of annotations in spacy format: [example text, {'entities': [(span start, span end, label)]}]
     """
-    
+
     def __init__(
         self,
         *,
         model=None,
         labels,
-        delimiter=',',
-        attr="LOWER", # "ORTH"
+        delimiter=",",
+        attr="LOWER",  # "ORTH"
         include_skip=True,
     ):
         self.model = model
@@ -50,11 +53,11 @@ class Annotator: # TODO (object) ?
         else:
             # TODO think of better solution for default?
             self.nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser"])
-        self.labels = labels # TODO check spacy terminology
+        self.labels = labels  # TODO check spacy terminology
         self.delimiter = delimiter
         self.attr = attr
         self.include_skip = include_skip
-    
+
     @property
     def instructions(self):
         print(
@@ -69,28 +72,37 @@ class Annotator: # TODO (object) ?
             \t * finish terminates the annotation session.
             """
         )
-        
+
     def _load_data(self, df, sample_size=1, shuffle=False, strata=None):
-        if 'annotations' in df.columns:
-            raise Exception("Dataframe already has an annotations column, I don't want to overwrite this.")
+        if "annotations" in df.columns:
+            raise Exception(
+                "Dataframe already has an annotations column, I don't want to overwrite this."
+            )
         df_out = df.copy()
         if strata is not None:
-            assert sum([v for k,v in strata.items() if k !='key']) == 1, 'The sum of proportions in strata is different from 1'
-            df_out = (df_out
-                      .groupby(strata['key'], group_keys=False)
-                      .apply(lambda x: x.sample(frac=(len(df) * sample_size * strata[x.name] / len(x))))
-                     ).reset_index(drop=True)
+            assert (
+                sum([v for k, v in strata.items() if k != "key"]) == 1
+            ), "The sum of proportions in strata is different from 1"
+            df_out = (
+                df_out.groupby(strata["key"], group_keys=False).apply(
+                    lambda x: x.sample(
+                        frac=(len(df) * sample_size * strata[x.name] / len(x))
+                    )
+                )
+            ).reset_index(drop=True)
 
         elif (sample_size != 1) or shuffle:
             df_out = df_out.sample(frac=sample_size).reset_index(drop=True)
-        df_out['annotations'] = ''
+        df_out["annotations"] = ""
         return df_out
-    
+
     def __add_annotation(self, df, col_text, current_index, annotations):
         spans = []
         for label, items in annotations.items():
             if items:
-                item_list = [i.strip() for i in items.split(self.delimiter) if i.strip() != ""]
+                item_list = [
+                    i.strip() for i in items.split(self.delimiter) if i.strip() != ""
+                ]
                 print(item_list)
                 matcher = PhraseMatcher(self.nlp.vocab, attr=self.attr)
                 matcher.add(label, [self.nlp(item) for item in item_list])
@@ -104,30 +116,27 @@ class Annotator: # TODO (object) ?
                     spans_new.append(span)
                 spans_filtered = spacy.util.filter_spans(spans_new)
                 print(spans_filtered)
-                spans.extend([(span.start_char, span.end_char, label) for span in spans_filtered])
+                spans.extend(
+                    [(span.start_char, span.end_char, label) for span in spans_filtered]
+                )
             else:
                 continue
-        entities = {'entities': spans}
-        df.at[current_index, 'annotations'] = (df[col_text][current_index], entities)
-    
-    def annotate(
-        self, 
-        *,
-        df, 
-        col_text,
-        show_instructions=False, 
-        **kwargs
-    ):
+        entities = {"entities": spans}
+        df.at[current_index, "annotations"] = (df[col_text][current_index], entities)
+
+    def annotate(self, *, df, col_text, show_instructions=False, **kwargs):
         ## CHECK INPUTS ----
-        
-        assert col_text is not None, 'Please provide a name of column containing text to be labelled.'
+
+        assert (
+            col_text is not None
+        ), "Please provide a name of column containing text to be labelled."
 
         ## PRE-PROCESS DATA ---
-        
+
         sample = self._load_data(df, **kwargs)
-        
+
         ## IPYWIDGET FUNCS ----
-            
+
         def skip(btn):
             show_next()
 
@@ -135,19 +144,24 @@ class Annotator: # TODO (object) ?
             for btn in buttons:
                 btn.disabled = True
             return
-        
+
         def submit(btn):
-            self.__add_annotation(sample, col_text, current_index, {t.description: t.value for t in textboxes.values()})
+            self.__add_annotation(
+                sample,
+                col_text,
+                current_index,
+                {t.description: t.value for t in textboxes.values()},
+            )
             for textbox in textboxes.values():
                 textbox.value = ""
             show_next()
-            
+
         def set_label_text():
             nonlocal count_label
-            count_label.value = f'{current_index} examples annotated, {len(sample) - current_index} examples left'
-            
+            count_label.value = f"{current_index} examples annotated, {len(sample) - current_index} examples left"
+
         def reset_textarea():
-            value = (';\n'.join(f'{label}: insert' for label in self.labels)+';')
+            value = ";\n".join(f"{label}: insert" for label in self.labels) + ";"
             return value
 
         def show_next():
@@ -159,65 +173,68 @@ class Annotator: # TODO (object) ?
                     btn.disabled = True
                 with out:
                     clear_output(wait=True)
-                    print ("\033[1mThat's all folks!\033[0m\n")
+                    print("\033[1mThat's all folks!\033[0m\n")
             else:
                 with out:
                     clear_output(wait=True)
-                    print ('\033[1mText:\033[0m')
+                    print("\033[1mText:\033[0m")
                     doc = self.nlp(sample[col_text][current_index])
                     if self.model is None:
                         doc.ents = []
                     for label in textboxes.keys():
-                        textboxes[label].value = ", ".join([ent.text for ent in doc.ents if ent.label_ == label])
+                        textboxes[label].value = ", ".join(
+                            [ent.text for ent in doc.ents if ent.label_ == label]
+                        )
                     # TODO remove warning of no ents on first doc
                     # TODO remove null
                     html = displacy.render(doc, style="ent")
                     display_html(html, raw=True)
-                    print('')
+                    print("")
             # TODO check out nlp.entity.beam_parse in spacy_annotator.pandas_annotations.annotate
             # understand threshold used by default, etc.
             # see https://stackoverflow.com/questions/46934523/spacy-ner-probability
 
         ## IPYWIDGET ----
-        
+
         if show_instructions:
             self.instructions
-        
+
         buttons = []
-        
-        btn = Button(description='submit', button_style="success")
+
+        btn = Button(description="submit", button_style="success")
         btn.on_click(submit)
         buttons.append(btn)
 
         if self.include_skip:
-            btn = Button(description='skip', button_style="danger")
+            btn = Button(description="skip", button_style="danger")
             btn.on_click(skip)
             buttons.append(btn)
-            
-#         btn = Button(description='previous')
-#         # TODO add "previous" button, cf pigeonXT
-#         buttons.append(btn)
-        
-        btn = Button(description='finish')
+
+        #         btn = Button(description='previous')
+        #         # TODO add "previous" button, cf pigeonXT
+        #         buttons.append(btn)
+
+        btn = Button(description="finish")
         btn.on_click(finish)
         buttons.append(btn)
-        
+
         current_index = -1
         count_label = HTML()
-        
+
         set_label_text()
         display(count_label)
 
         # TODO rename to meaningful names: entities, text, buttons, etc.
-        textboxes = {label:
-                     Text(
-                         value='',
-                         description=f'{label}',
-                         placeholder=f'ent one{self.delimiter} ent two{self.delimiter} ent three',
-                         #     disabled=False,
-                         layout=Layout(width="auto")
-                     ) for label in self.labels
-                    }
+        textboxes = {
+            label: Text(
+                value="",
+                description=f"{label}",
+                placeholder=f"ent one{self.delimiter} ent two{self.delimiter} ent three",
+                #     disabled=False,
+                layout=Layout(width="auto"),
+            )
+            for label in self.labels
+        }
         display(*textboxes.values())
 
         box = HBox(buttons)
